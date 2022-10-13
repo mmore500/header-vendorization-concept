9d379462e041d5d06f74876d1d6985ceed51e21c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-10-12 20:13:42 -0700

    Setup compile recipes

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	g++ with_seep.cpp -o with_seep.out
+	g++ without_seep.cpp -o without_seep.out
---
+++ with_seep.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "ext/print_macro.hpp"
+
+#include "include/mylib/do_print_macro.hpp"
+#include "include/mylib/do_return_macro.hpp"
+
+int main() {
+
+  mylib::do_print_macro();
+
+  ext::print_macro();
+
+  std::cout << mylib::do_return_macro() << '\n';
+
+  #ifdef MYLIB_THIS_1ST_EXT_MACRO
+  std::cout << "MYLIB_THIS_1ST_EXT_MACRO" << '\n';
+  #endif
+
+  #ifdef MYLIB_THAT_1ST_EXT_MACRO
+  std::cout << "MYLIB_THAT_1ST_EXT_MACRO" << '\n';
+  #endif
+
+}
