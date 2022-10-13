4df1d0c0952e312b7253379fb17bf17b64d6767a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-10-12 20:29:30 -0700

    Automatically create facade headers

---
+++ include/mylib_ext/_codegen/make_facade.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3.10
+
+from glob import glob
+import os
+import re
+import textwrap
+import typing
+
+def iter_header_paths() -> typing.Iterator[str]:
+    yield from glob(
+        "**/*.hpp",
+        recursive=True,
+        root_dir="../../third-party/ext",
+    )
+
+def setup_one_facade(header_path: str) -> None:
+    header_guard = (
+        "MYLIB_EXT_"
+        + header_path.upper().replace("/", "_").replace(".", "_")
+    )
+    content = textwrap.dedent(f"""\
+        #ifndef {header_guard}
+        #define {header_guard}
+
+        #include "detail/push_mylib_ext.hh"
+
+        #include "../../third-party/ext/{header_path}"
+
+        #include "detail/pop_mylib_ext.hh"
+
+        #endif // #ifndef {header_guard}
+    """)
+
+    if os.path.dirname(header_path):
+        os.makedirs(os.path.dirname(header_path), exist_ok=True)
+    with open(header_path, "w") as header_file:
+        header_file.write(content)
+
+for header_path in iter_header_paths():
+    setup_one_facade(header_path)
